4522f31b3fafa7d99a676aa5a014b5e6a44fe94e
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-04-06 02:11:32 +0530

    NQueens: Count Ways& Print One Sol

---
+++ BackTracking/NQueensCountWays.java
@@ -0,0 +1,67 @@
+package BackTracking;
+
+public class NQueensCountWays {
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertically up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        //right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void nQueens(char board[][], int row) {
+        //base
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        //looping through column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = 'X';
+            }
+        }
+    }
+
+    static int count = 0;
+    
+    
+    public static void main(String[] args) {
+        int n = 5;
+        char board[][] = new char[n][n];
+
+        // initialize
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        //function call
+        nQueens(board, 0);
+        System.out.println("Total ways to solve N-Queens: " + count);
+        
+
+    }
+}
---
+++ BackTracking/NQueensPrintOneSol.java
@@ -0,0 +1,84 @@
+package BackTracking;
+
+public class NQueensPrintOneSol {
+
+    //CHECK IF PROBLEM CAN BE SOLVED AND PRINT ONLY ONE SOLUTION
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertically up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        //right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean nQueens(char board[][], int row) {
+        //base
+        if (row == board.length) {
+            return true;
+        }
+
+        //looping through column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                if (nQueens(board, row + 1)) {
+                    return true;
+                }
+                board[row][j] = 'X';
+            }
+        }
+
+        return false;
+    }
+    
+    public static void printBoard(char board[][]) {
+        System.out.println("-----Chess Board-----");
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        int n = 5;
+        char board[][] = new char[n][n];
+
+        // initialize
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        //function call
+        if (nQueens(board, 0)) {
+            System.out.println("Solution is possible");
+            printBoard(board);
+        }
+        else {
+            System.out.println("Solution is not possible");
+        }
+
+    }
+}
