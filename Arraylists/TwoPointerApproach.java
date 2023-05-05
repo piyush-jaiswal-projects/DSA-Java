49030ba1f215a3783ca1625fd239c3d51a56fc64
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-05-05 09:30:16 +0530

    Two Pointer Approach

---
+++ Arraylists/TwoPointerApproach.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class TwoPointerApproach {
+
+    public static int maxWater(ArrayList<Integer> heights) {
+        int maxWater = 0;
+        int lp = 0, rp = heights.size() - 1;
+
+        while (lp < rp) {
+            int ht = Math.min(heights.get(lp), heights.get(rp));
+            int wt = rp - lp;
+            int currWater = ht * wt;
+            maxWater = Math.max(maxWater, currWater);
+
+            if (lp < rp) {
+                lp++;
+            }
+            else {
+                rp--;
+            }
+        }
+        
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> heights = new ArrayList<>();
+        //[1,8,6,2,5,4,8,3,7]
+        heights.add(1);
+        heights.add(8);
+        heights.add(6);
+        heights.add(2);
+        heights.add(5);
+        heights.add(4);
+        heights.add(8);
+        heights.add(3);
+        heights.add(7);
+
+        System.out.println(maxWater(heights));
+    }
+}
+
