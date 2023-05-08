599e5558e7993a7b9a69095f1be455b96330e658
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-05-08 19:54:56 +0530

    Remove Cycle from LL

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -232,12 +232,46 @@ public class LinkedList {
         return false; //cycle doesn't exists
     }
 
+    public static void removeCycle() {
+        //detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null & fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        //find cycle creation node and its prev node
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //point prev next to null
+        prev.next = null;
+    }
+
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        //1->2->3->1
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = temp;
+        //1->2->3->4->2
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 }
