6cc168120ef8f502c1270ec642b47faa4a07ce0a
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-05-09 22:08:52 +0530

    Stacks Using LinkedList

--- Stacks/StackB.java
+++ Stacks/StackB.java
@@ -4,24 +4,30 @@ public class StackB {
     static class Stack {
         static ArrayList<Integer> list = new ArrayList<>();
 
-        public static boolean isEmpty() {
+        public  boolean isEmpty() {
             return list.size() == 0;
         }
 
         //push
-        public static void push(int data) {
+        public void push(int data) {
             list.add(data);
         }
 
         //pop
-        public static int pop() {
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
             int val = list.get(list.size() - 1);
             list.remove(list.size() - 1);
             return val;
         }
 
         //peek
-        public static int peek() {
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
             return list.get(list.size() - 1);
         }
     }
