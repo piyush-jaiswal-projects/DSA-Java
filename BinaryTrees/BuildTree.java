4d900af114ecbfd7f6086b0ad8da94b6a3a76464
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-24 22:17:17 +0530

    Inorder and Postorder traversal

--- BinaryTrees/BuildTree.java
+++ BinaryTrees/BuildTree.java
@@ -39,6 +39,27 @@ public class BuildTree {
             preorder(root.left);
             preorder(root.right);
         }
+
+        public static void inorder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+
+            preorder(root.left);
+            System.out.print(root.data + " ");
+            preorder(root.right);
+        }
+
+        public static void postorder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+            preorder(root.left);
+            preorder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
@@ -46,5 +67,9 @@ public class BuildTree {
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         tree.preorder(root);
+        System.out.println();
+        tree.inorder(root);
+        System.out.println();
+        tree.postorder(root);
     }
 }
