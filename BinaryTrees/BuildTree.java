b2794953abc15b07786e0c2abcd42627683dbf17
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-24 21:46:04 +0530

    Preorder Traversal

--- BinaryTrees/BuildTree.java
+++ BinaryTrees/BuildTree.java
@@ -29,12 +29,22 @@ public class BuildTree {
 
             return newNode;
         }
+
+        public static void preorder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        tree.preorder(root);
     }
 }
