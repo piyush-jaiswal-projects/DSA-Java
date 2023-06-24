7ba5020735e6cb2e46fb095db405e3a1a80385f8
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-24 22:30:15 +0530

    Level Order Traversal

--- BinaryTrees/BuildTree.java
+++ BinaryTrees/BuildTree.java
@@ -1,5 +1,7 @@
 package BinaryTrees;
 
+import java.util.*;
+
 public class BuildTree {
 
     static class Node {
@@ -60,16 +62,45 @@ public class BuildTree {
             preorder(root.right);
             System.out.print(root.data + " ");
         }
+
+        public static void level(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                }
+                else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+
+
+        }
     }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        tree.preorder(root);
-        System.out.println();
-        tree.inorder(root);
-        System.out.println();
-        tree.postorder(root);
+        tree.level(root);
     }
 }
